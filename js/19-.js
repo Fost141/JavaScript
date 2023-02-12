84b6679c9745c48e8e361bc41a73007719878894
Georg <example@gmail.com>
2023-02-12 19:16:13 +0700

    less 19

--- js/19-.js
+++ js/19-.js
@@ -1,21 +1,82 @@
-// Урок 19 практическое задание
-let number0fFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
+// !Урок 19 практическое задание
+// let number0fFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
 
-let personalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    generes: [],
-    privat: false
-};
+// let personalMovieDB = {
+//     count: number0fFilms,
+//     movies: {},
+//     actors: {},
+//     generes: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильсов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильсов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильсов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильсов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b; // Что значит эта строчка?
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+// Вывод информации из объекта в тег <p>
+// const films = {
+//     name: {},
+//     ball: {}
+// };
+
+// const n = prompt('Название фильма', ''),
+//       b = prompt('Оцените фильм', '');
+
+// films.name = n;
+// films.ball = b;
+
+// console.log(films);
+
+// let enter = document.querySelector('.enter').innerHTML = 'Name film' + ': ' + films.name + ' ' + 'Ball film' + ': ' + films.ball;
+
+
+// !Урок 20 Условия
+// if (4 == 9) {
+//     console.log('ok');
+// } else {
+//     console.log('error');
+// }
+
+// let a = prompt('Number', '');
+// if (a == 4) {
+//     console.log('yes');
+// } else {
+//     console.log('no');
+// }
+
+// alert('Вычисляем умножение')
+// let a = +prompt('Введите первое число', '');
+// let b = +prompt('Введите второе число', '');
+
+// console.log(a * b);
+
+//Тернарный оператор  знак - ? и :
+// let num = 50;
+// (num == 50) ? console.log('ok') : console.log('error');
+
+//Switch
+// let num = 5;
+// switch (num) {
+//     case 49:
+//         console.log('error');
+//         break;
+//     case 100:
+//         console.log('error');
+//         break;
+//     case 50:
+//         console.log('ok');
+//         break;
+//     default: 
+//         console.log('next time');
+//         break;
+// };
+
+
     
